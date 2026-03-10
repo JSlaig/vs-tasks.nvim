e448f2d4d315be3de26627679c430d876e8564d9
JSlaig <jsaligz@gmail.com>
2026-03-10 23:46:31 +0100

    AI fix provisional command on clipboard

--- lua/vstask/Floaterm.lua
+++ lua/vstask/Floaterm.lua
@@ -4,9 +4,10 @@
 local function Floaterm_process(command)
   -- Open a new Floaterm terminal (floaterm is pre-configured)
   vim.cmd("FloatermOpenInNewTerm")
-  
-  -- Execute the command in the newly opened terminal
-  vim.cmd([[TermExec cmd="]] .. command .. [["]])
+
+  -- Put the command in the clipboard (register "*")
+  -- This allows you to paste it with p or P
+  vim.fn.setreg("*", command)
 end
 
 return { Process = Floaterm_process }
